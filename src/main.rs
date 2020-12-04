535db9414083b6b5d846221f76995236ef698654
Daniel Johnson <35911630+Witherino@users.noreply.github.com>
2020-12-04 15:54:48 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -120,22 +120,22 @@ fn main() {
     nwg::dispatch_thread_events();
 }
 
-fn main() {
-    // Create a new blockchain (we would ideally read from disk and update from P2P)
-    let mut blockchain = Blockchain::new(2);
-    // Peers (peer id) doing transaction
-    let alice = 33;
-    let bob = 49;
-    // Create the transaction
-    let transaction = Transaction::new(alice, bob, 100);
-    // Add the transaction to the blockchain
-    blockchain.add_transaction(transaction).expect("Failed to add new transaction");
-    // Make another transaction in the opposite direction
-    blockchain.add_transaction(Transaction::new(bob, alice, 50)).unwrap();
-    // Print out all blocks
-    let serial = serde_json::to_string(&blockchain).unwrap();
-    println!("Testing serialization: {}", serial);
-    let deserial: Value = serde_json::from_str(&serial).unwrap();
-    println!("Testing deserialization: {}", deserial);
-    for block in blockchain.block_chain() {
-        println!("Block {:?}", block);
+// fn main() {
+//     // Create a new blockchain (we would ideally read from disk and update from P2P)
+//     let mut blockchain = Blockchain::new(2);
+//     // Peers (peer id) doing transaction
+//     let alice = 33;
+//     let bob = 49;
+//     // Create the transaction
+//     let transaction = Transaction::new(alice, bob, 100);
+//     // Add the transaction to the blockchain
+//     blockchain.add_transaction(transaction).expect("Failed to add new transaction");
+//     // Make another transaction in the opposite direction
+//     blockchain.add_transaction(Transaction::new(bob, alice, 50)).unwrap();
+//     // Print out all blocks
+//     let serial = serde_json::to_string(&blockchain).unwrap();
+//     println!("Testing serialization: {}", serial);
+//     let deserial: Value = serde_json::from_str(&serial).unwrap();
+//     println!("Testing deserialization: {}", deserial);
+//     for block in blockchain.block_chain() {
+//         println!("Block {:?}", block);
